cd5a9f9156fa02881c7bc3b918413d7f377bab4e
wenhuizhang <wenhui@gwmail.gwu.edu>
2020-10-14 00:00:51 -0400

    add test

---
+++ test/creat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>  /* Many POSIX functions (but not all, by a large margin) */
+#include <sys/stat.h>
+#include <fcntl.h>   /* open(), creat() - and fcntl() */
+
+
+
+int main(int argc, char **argv)
+{
+	int i;
+	const char *name = "tmp";
+
+
+	creat(name, 0777);
+
+	return 0;
+}
+
